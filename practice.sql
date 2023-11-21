21ad17696bc9617c7e0934cd9e96480e78f6dc43
mayanksingh-0405 <mayanksingh13343@gmail.com>
2023-11-21 12:37:59 +0530

    Added an SQL file

---
+++ practice.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE myrestraunt;
+USE myrestraunt;
+CREATE TABLE users(
+    id INTEGER NOT NULL PRIMARY KEY,
+    name varchar(30) NOT NULL,
+    phone varchar(50),
+    address varchar(300),
+    password VARCHAR(50)
+);
+
+INSERT INTO users(id,name,phone,address,password)
+VALUES
+(1,'Gaurav','123456789','Mumbai,India','pass134'),
+(2,'Sakshi','987654321','Chennai,India','pass456');
+
+SELECT * FROM users;
